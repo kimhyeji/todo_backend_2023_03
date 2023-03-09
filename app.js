6b3dc4dd8b2a3984867c4158f92b4821515177d2
kimhyeji <gpwl5589@gmail.com>
2023-03-09 19:29:27 +0900

    할일 조회 API

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 import express from "express";
 import cors from "cors";
+import mysql from "mysql2/promise";
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "korad",
+  password: "kor123414",
+  database: "todo_2023_03",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  dateStrings: true,
+});
 
 const app = express();
 
 const corsOptions = {
-  origin: "https://naver.io",
+  origin: "https://cdpn.io",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
@@ -12,12 +24,24 @@ app.use(cors(corsOptions));
 
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.get("/:user_code/todos", async (req, res) => {
+  const { user_code } = req.params;
 
-app.get("/todos", (req, res) => {
-  res.json([{ id: 1 }, { id: 2 }]);
+  const [rows] = await pool.query(
+    `
+    SELECT *
+    FROM todo
+    WHERE user_code = ?
+    ORDER BY id DESC
+    `,
+    [user_code]
+  );
+
+  res.json({
+    resultCode: "S-1",
+    msg: "성공",
+    data: rows,
+  });
 });
 
 app.listen(port, () => {
